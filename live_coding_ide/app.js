51e0305143330336b5d9de2388671fffd33f556b
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2017-07-01 17:01:52 +0200

    Fix indentation (autoindent to 4)

--- live_coding_ide/app.js
+++ live_coding_ide/app.js
@@ -1,164 +1,162 @@
 (function() {
-  this.vimInsertMode = false
-  this.pendingSuccessfulCodeLoadAfterError = false
-  this.liveCodeState = null
-  this.liveCodeVersion = 1
+    this.vimInsertMode = false
+    this.pendingSuccessfulCodeLoadAfterError = false
+    this.liveCodeState = null
+    this.liveCodeVersion = 1
+    this.liveViewElement = null
 
-  this.liveViewElement = null
+    init = () => {
+        setUpLiveView()
+        setUpConsole()
 
+        let editor = setUpEditor()
+        loadSavedCode(editor)
 
-  init = () => {
-    setUpLiveView()
-    setUpConsole()
+        setUpEditorControlUI()
+    }
 
-    let editor = setUpEditor()
-    loadSavedCode(editor)
+    this.setUpEditorControlUI = () => {
+        let node = document.getElementsByClassName("js-editor-controls")[0]
+        let settings = localStorage.getItem("settings")
 
-    setUpEditorControlUI()
-  }
+        if(settings) { settings = JSON.parse(settings) }
 
-  this.setUpEditorControlUI = () => {
-    let node = document.getElementsByClassName("js-editor-controls")[0]
-    let settings = localStorage.getItem("settings")
+        // Throw away old incompatible data, remove after juli 2017
+        if(settings && settings.githubProjectPath) { settings = null }
 
-    if(settings) { settings = JSON.parse(settings) }
+        let app = Elm.Main.embed(node, settings)
 
-    // Throw away old incompatible data, remove after juli 2017
-    if(settings && settings.githubProjectPath) { settings = null }
+        app.ports.saveSettings.subscribe((settings) => {
+            localStorage.setItem("settings", JSON.stringify(settings))
+        })
 
-    let app = Elm.Main.embed(node, settings)
+        app.ports.loadCodeFromGithub.subscribe((url) => {
+            fetchFromUrl(url, function(body) {
+                // https://stackoverflow.com/questions/22587308/convert-iso-8859-1-to-utf-8
+                code = decodeURIComponent(escape(atob(JSON.parse(body).content)))
 
-    app.ports.saveSettings.subscribe((settings) => {
-      localStorage.setItem("settings", JSON.stringify(settings))
-    })
+                replaceCodeInEditor(code)
 
-    app.ports.loadCodeFromGithub.subscribe((url) => {
-      fetchFromUrl(url, function(body) {
-        // https://stackoverflow.com/questions/22587308/convert-iso-8859-1-to-utf-8
-        code = decodeURIComponent(escape(atob(JSON.parse(body).content)))
+                // Reload page after replacing code as this is often required to get it running
+                window.location.reload()
+            })
+        })
 
-        replaceCodeInEditor(code)
+        app.ports.loadCodeFromGist.subscribe((url) => {
+            fetchFromUrl(url, function(body) {
+                files = Object.values(JSON.parse(body).files)
 
-        // Reload page after replacing code as this is often required to get it running
-        window.location.reload()
-      })
-    })
+                if(files.length != 1) {
+                    alert("Importing more than one file in a gist isn't supported yet.")
+                }
+                else {
+                    replaceCodeInEditor(files[0].content)
 
-    app.ports.loadCodeFromGist.subscribe((url) => {
-       fetchFromUrl(url, function(body) {
-         files = Object.values(JSON.parse(body).files)
+                    // Reload page after replacing code as this is often required to get it running
+                    window.location.reload()
+                }
+            })
+        })
+    }
 
-         if(files.length != 1) {
-           alert("Importing more than one file in a gist isn't supported yet.")
-         }
-         else {
-           replaceCodeInEditor(files[0].content)
+    this.setUpLiveView = () => {
+        this.liveViewElement = document.getElementsByClassName("js-view")[0]
+    }
 
-           // Reload page after replacing code as this is often required to get it running
-           window.location.reload()
-         }
-       })
-    })
-  }
+    this.setUpConsole = () => {
+        let consoleElement = document.getElementsByClassName("js-console")[0]
 
-  this.setUpLiveView = () => {
-    this.liveViewElement = document.getElementsByClassName("js-view")[0]
-  }
+        // Catch runtime errors (code loading errors are handled in runCode)
+        window.onerror = function(e) { console.log("LiveCoding: " + e) };
 
-  this.setUpConsole = () => {
-    let consoleElement = document.getElementsByClassName("js-console")[0]
+        browserLog = window.console.log
+        window.console.log = function(data, type) {
+            if(data instanceof Error) {
+                data = data.stack.split(" at ")[1] + data
+            } else if(typeof data === "object") {
+                try {
+                    data = JSON.stringify(data)
+                } catch(TypeError) {
+                    browserLog(data)
+                    console.log("LiveCoding: ^ Also logged this message to console since LiveCoding does not know how to display it.")
+                }
+            }
 
-    // Catch runtime errors (code loading errors are handled in runCode)
-    window.onerror = function(e) { console.log("LiveCoding: " + e) };
-
-    browserLog = window.console.log
-    window.console.log = function(data, type) {
-      if(data instanceof Error) {
-        data = data.stack.split(" at ")[1] + data
-      } else if(typeof data === "object") {
-        try {
-          data = JSON.stringify(data)
-        } catch(TypeError) {
-          browserLog(data)
-          console.log("LiveCoding: ^ Also logged this message to console since LiveCoding does not know how to display it.")
+            consoleElement.innerHTML = data + "<br/>" + consoleElement.innerHTML
         }
-      }
-
-      consoleElement.innerHTML = data + "<br/>" + consoleElement.innerHTML
-    }
-  }
-
-  this.setUpEditor = () => {
-    let editorDiv = document.getElementsByClassName("js-editor")[0]
-    let editor = ace.edit(editorDiv)
-
-    // TODO: Remove this when possible, the platform game relies on it
-    window.editor = editor
-
-    //editor.setTheme("ace/theme/github") // Light theme
-    editor.setTheme("ace/theme/twilight") // Dark theme
-
-    // Settings for javascript editing
-    editor.setOption("tabSize", 4)
-    editor.setOption("useSoftTabs", true)
-    editor.session.setMode("ace/mode/javascript")
-
-    editor.session.setUseWrapMode(true)
-
-    // Hide the 80 chars line. No strong reason, mostly because
-    // the size of the window is probably a more practical limit for now.
-    editor.setShowPrintMargin(false)
-
-    if(window.location.href.indexOf("vim") != -1) {
-      editor.setKeyboardHandler("ace/keyboard/vim")
     }
 
-    // Have no looked deeply at this:
-    // "Automatically scrolling cursor into view after selection change this will be disabled in the next version set editor.$blockScrolling = Infinity to disable this message"
-    editor.$blockScrolling = Infinity
+    this.setUpEditor = () => {
+        let editorDiv = document.getElementsByClassName("js-editor")[0]
+        let editor = ace.edit(editorDiv)
 
-    editor.on("changeStatus", function() {
-      if(editor.keyBinding.getStatusText(editor) == "INSERT") { vimInsertMode = true }
-      else { if(vimInsertMode) { vimInsertMode = false; runCode(editor) } }
-    })
-    editor.on("change", () => { runCode(editor) });
+        // TODO: Remove this when possible, the platform game relies on it
+        window.editor = editor
 
-    return editor
-  }
+        //editor.setTheme("ace/theme/github") // Light theme
+        editor.setTheme("ace/theme/twilight") // Dark theme
 
-  this.loadSavedCode = (editor) => {
-    savedCode = localStorage.getItem("code")
+        // Settings for javascript editing
+        editor.setOption("tabSize", 4)
+        editor.setOption("useSoftTabs", true)
+        editor.session.setMode("ace/mode/javascript")
 
-    if(savedCode) {
-      console.log("LiveCoding: Loading previously saved code from local storage")
-      editor.setValue(savedCode)
+        editor.session.setUseWrapMode(true)
 
-      // Reset undo stack to the saved code instead of the default example :)
-      editor.session.setUndoManager(new ace.UndoManager())
+        // Hide the 80 chars line. No strong reason, mostly because
+        // the size of the window is probably a more practical limit for now.
+        editor.setShowPrintMargin(false)
 
-      editor.clearSelection()
-    } else {
-      runCode(editor)
+        if(window.location.href.indexOf("vim") != -1) {
+            editor.setKeyboardHandler("ace/keyboard/vim")
+        }
+
+        // Have no looked deeply at this:
+        // "Automatically scrolling cursor into view after selection change this will be disabled in the next version set editor.$blockScrolling = Infinity to disable this message"
+        editor.$blockScrolling = Infinity
+
+        editor.on("changeStatus", function() {
+            if(editor.keyBinding.getStatusText(editor) == "INSERT") { vimInsertMode = true }
+            else { if(vimInsertMode) { vimInsertMode = false; runCode(editor) } }
+        })
+        editor.on("change", () => { runCode(editor) });
+
+        return editor
     }
 
-    editor.gotoLine(1)
-    editor.focus()
-  }
+    this.loadSavedCode = (editor) => {
+        savedCode = localStorage.getItem("code")
 
-  this.runCode = (editor) => {
-    if(vimInsertMode) { return }
+        if(savedCode) {
+            console.log("LiveCoding: Loading previously saved code from local storage")
+            editor.setValue(savedCode)
 
-    code = editor.getValue()
+            // Reset undo stack to the saved code instead of the default example :)
+            editor.session.setUndoManager(new ace.UndoManager())
 
-    script = document.createElement("script")
-    script.type = "text/javascript"
+            editor.clearSelection()
+        } else {
+            runCode(editor)
+        }
 
-    localStorage.setItem("code", code)
+        editor.gotoLine(1)
+        editor.focus()
+    }
 
-    window.liveCodeVersion += 1
+    this.runCode = (editor) => {
+        if(vimInsertMode) { return }
 
-    script.innerHTML =
-      "(function() {" +
+        code = editor.getValue()
+
+        script = document.createElement("script")
+        script.type = "text/javascript"
+
+        localStorage.setItem("code", code)
+
+        window.liveCodeVersion += 1
+
+        script.innerHTML =
+        "(function() {" +
         // Add function that can be used to check if the code is outdated and should stop running.
         "function codeHasChanged() { return " + this.liveCodeVersion + " != this.liveCodeVersion };" +
 
@@ -171,31 +169,31 @@
 
         "\nif(this.pendingSuccessfulCodeLoadAfterError) { this.pendingSuccessfulCodeLoadAfterError = false; console.log('LiveCoding: Code now loads successfully again!') }" +
         " } catch(e) { this.pendingSuccessfulCodeLoadAfterError = true; console.log(e) }" +
-      "})();"
+        "})();"
 
-    document.body.appendChild(script)
-  }
-
-  this.fetchFromUrl = (url, callback) => {
-    let xmlhttp = new XMLHttpRequest()
-
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        callback(xmlhttp.responseText)
-      } else if (xmlhttp.status != 0 && xmlhttp.status != 200) {
-        console.log("Failed to get " + url + ": " + xmlhttp.responseText + " [" + xmlhttp.status + "]")
-      }
+        document.body.appendChild(script)
     }
 
-    xmlhttp.open("GET", url, true)
-    xmlhttp.send()
-  }
+    this.fetchFromUrl = (url, callback) => {
+        let xmlhttp = new XMLHttpRequest()
 
-  this.replaceCodeInEditor = (code) => {
-    editor.setValue(code)
-    editor.gotoLine(1)
-    editor.focus()
-  }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                callback(xmlhttp.responseText)
+            } else if (xmlhttp.status != 0 && xmlhttp.status != 200) {
+                console.log("Failed to get " + url + ": " + xmlhttp.responseText + " [" + xmlhttp.status + "]")
+            }
+        }
 
-  init()
+        xmlhttp.open("GET", url, true)
+        xmlhttp.send()
+    }
+
+    this.replaceCodeInEditor = (code) => {
+        editor.setValue(code)
+        editor.gotoLine(1)
+        editor.focus()
+    }
+
+    init()
 }())
