dafbe3904c3cdae7689f94c2bf18434d8abb62c3
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2017-07-03 11:42:05 +0200

    Update todos

--- live_coding_ide/ui/Main.elm
+++ live_coding_ide/ui/Main.elm
@@ -1,15 +1,12 @@
 module Main exposing (..)
 
 -- todo:
--- - See if code can be reloaded by having an iframe to blow away js-state, this would allow nicer Elm UI navigation
--- - Select project and see things for the active project
--- - Have Elm handle code loading, write to local cache and assume multiple files
--- - Save changes from editor through Elm
--- - Load code from local cache when switching projects
--- todo: redo UI, click project to see info and options, also naming for projects
---       delete, load remote, load local, has local changes?, change name, ...
---       handle code loading through Elm?
---       - clicking a project switches to it right away but does not load remote code
+-- identify projects by something other than remoteCodeUrl to allow local projects
+-- always have a project loaded, have a default one
+-- naming for projects
+-- clone projects
+-- be able to change the remoteCodeUrl
+-- rewrite more of the js code in elm, console, api calls?
 -- todo: backup and restore of locally persisted settings?
 
 import Types exposing (Settings, Model, Msg)
